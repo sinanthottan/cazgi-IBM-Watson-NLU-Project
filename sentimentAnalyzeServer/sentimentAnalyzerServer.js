5ede96f0e76324bb950bbba4a2ecaa0c9f6e140e
sinanthottan <sinanthottan@gmail.com>
2021-06-24 14:36:33 +0000

    updated end points

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -3,47 +3,134 @@ const app = new express();
 const dotenv = require('dotenv');
 dotenv.config();
 
-function getNLUInstance(){
-    let api_key = process.env.API_KEY;
-    let api_url = process.env.API_URL;
-
-    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understading/v1');
-    const { IamAuthenticator } = require('ibm-watson/auth');
-
-    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-        version: '2020-08-01',
-        authenticator: new IamAuthenticator({
-            apikey: api_key,
-        }),
-        serviceUrl: api_url,
-    });
-    return naturalLanguageUnderstanding;
-}
-
 app.use(express.static('client'))
 
 const cors_app = require('cors');
 app.use(cors_app());
 
+function getNLUInstance(){
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+  version: '2021-03-25',
+  authenticator: new IamAuthenticator({
+    apikey: api_key,
+  }),
+  serviceUrl: api_url,
+});
+
+   return naturalLanguageUnderstanding;
+}
+
+function textEmotion(textParam,res) {
+    let nluInstance = getNLUInstance();
+
+    const analyzeParams = {
+        'text': textParam,
+            'features': {
+                'emotion': {
+                
+                }
+            }
+    };
+    
+    nluInstance.analyze(analyzeParams)
+        .then(analysisResults => {
+            res.send(analysisResults);
+         })
+        .catch(err => {
+            console.log(err.toString());
+        });
+}
+
+function textSentiment(textParam,res) {
+    let nluInstance = getNLUInstance();
+
+    const analyzeParams = {
+        'text': textParam,
+            'features': {
+                'sentiment': {
+                
+                }
+            }
+    };
+    
+    nluInstance.analyze(analyzeParams)
+        .then(analysisResults => {
+            res.send(analysisResults);
+         })
+        .catch(err => {
+            console.log(err.toString());
+        });
+}
+function urlEmotion(urlParam,res) {
+    let nluInstance = getNLUInstance();
+
+    const analyzeParams = {
+        'url': urlParam,
+            'features': {
+                'emotion': {
+                
+                }
+            }
+    };
+    
+    nluInstance.analyze(analyzeParams)
+        .then(analysisResults => {
+            res.send(analysisResults);
+         })
+        .catch(err => {
+            console.log(err.toString());
+        });
+}
+
+function urlSentiment(urlParam,res) {
+    let nluInstance = getNLUInstance();
+
+    const analyzeParams = {
+        'url': urlParam,
+            'features': {
+                'sentiment': {
+                
+                }
+            }
+    };
+    
+    nluInstance.analyze(analyzeParams)
+        .then(analysisResults => {
+            res.send(analysisResults);
+         })
+        .catch(err => {
+            console.log(err.toString());
+        });
+}
 app.get("/",(req,res)=>{
     res.render('index.html');
   });
 
 app.get("/url/emotion", (req,res) => {
 
-    return res.send({"happy":"90","sad":"10"});
+    let urlParam = req.query.url;
+    urlEmotion(urlParam,res);
 });
 
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+    let urlParam = req.query.url;
+    urlSentiment(urlParam,res);
 });
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+    let textParam = req.query.text;
+    textEmotion(textParam,res);
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    let textParam = req.query.text;
+    textSentiment(textParam,res);
 });
 
 let server = app.listen(8080, () => {
